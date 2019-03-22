6beea1e10523f1a8285b9b6fd5aa539140b6eb7e
Niklas Seefeldt <mr.niklasseefeldt@gmail.com>
2019-03-23 00:31:43 +0100

    Cambios en la estructura del código y los nombres de las variables.

--- 2018-2019/Fundamentals Programming/TEMA_02/T2_EJ_08.cpp
+++ 2018-2019/Fundamentals Programming/TEMA_02/T2_EJ_08.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 int main(){
 
-    int n1, n2;
+    //Declaramos las variables de tipo entero "number1" para el primer número y "number2" para el segundo número.
+    int number1, number2;
 
-    cout << "\n\n\tIntroduzca el numero 1: ";   cin >> n1;
-    cout << "\tIntroduzca el numero 2: ";   cin >> n2;
-    cout << "\n\tSuma: " << n1 << " + " << n2 << " = " << n1+n2 << endl
-         << "\tResta: " << n1 << " - " << n2 << " = " << n1-n2 << endl
-         << "\tProducto: " << n1 << " * " << n2 << " = " << n1*n2 << endl
-         << "\tCociente: " << n1 << " / " << n2 << " = " << (float) n1/n2 << "\n" << endl;
+    //Introducimos los valores "number1" y "number2".
+    cout << "\n\n\tIntroduzca el numero 1: ";cin >> number1;
+    cout << "\tIntroduzca el numero 2: "; cin >> number2;
+
+    //Ahora escribimos todas las operaciones que debe realizar.
+    cout << "\n\tSuma: " << number1 << " + " << number2 << " = " << number1+number2 << endl
+         << "\tResta: " << number1 << " - " << number2 << " = " << number1-number2 << endl
+         << "\tProducto: " << number1 << " * " << number2 << " = " << number1*number2 << endl
+         << "\tCociente: " << number1 << " / " << number2 << " = " << (float) number1/number2 << "\n" << endl;
 
     cout.precision(3); //Sirve para redondear/truncar (2 decimales si el numero entero es mayor que 0 y 3 decimales si el numero entero es 0).
     system ("pause");
