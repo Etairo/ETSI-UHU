a19acdbfba6ce8ac0bce392125439db0f366e3a8
Niklas Seefeldt <mr.niklasseefeldt@gmail.com>
2019-03-23 01:16:14 +0100

    Añadiendo algunos comentarios al código.

--- 2018-2019/Fundamentals Programming/TEMA_02/T2_EJ_10.cpp
+++ 2018-2019/Fundamentals Programming/TEMA_02/T2_EJ_10.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main(){
 
-    int radio;
+    int radio; //Declaramos las variables.
 
-    cout << "\n\n\tIntroduzca la longitud del radio: "; cin >> radio;
-    cout << "\n\tLongitud: " << 2*pi*radio << endl
-         << "\tSuperficie: " << pi*pow(radio,2) << endl
-         << "\tVolumen: " << (float) (4*pi*pow(radio,3))/3 << "\n" << endl;
+    cout << "\n\n\tIntroduzca la longitud del radio: "; cin >> radio; //Introducimos el valor del radio de la circunferencia.
+    cout << "\n\tLongitud: " << 2*pi*radio << endl //Obtenemos la longitud de la circunferencia.
+         << "\tSuperficie: " << pi*pow(radio,2) << endl //Obtenemos la superficie de la circunferencia.
+         << "\tVolumen: " << (float) (4*pi*pow(radio,3))/3 << "\n" << endl; //Obtenemos el volumen de la circunferencia.
 
     system ("pause");
     return 0;
